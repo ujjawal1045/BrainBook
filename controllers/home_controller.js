52c09a6b4db00e300b34e684cf27936840c9fb0b
ujjawal1045 <90901525+ujjawal1045@users.noreply.github.com>
2022-05-24 05:40:55 +0530

    created home controller and connected it with routes

---
+++ controllers/home_controller.js
@@ -0,0 +1,3 @@
+module.exports.home = function(req,res) {
+    return res.end('<h1>Express i s up for BrainBook!</h1>');
+}
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const homeController = require('../controllers/home_controller');
 console.log('router loaded successfully');
+
+//02//to access home controller action in routes
+router.get('/',homeController.home);
+
+
 module.exports = router;
