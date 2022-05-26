13d37c66af4f6a1950392b78d929b12ef3a385ce
ujjawal1045 <90901525+ujjawal1045@users.noreply.github.com>
2022-05-27 02:35:18 +0530

    created user through signup form and save the user in database to establish identitu

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,7 @@
 module.exports.home = function(req,res) {
     //return res.end('<h1>Express i s up for BrainBook!</h1>');
+    // console.log(req.cookies);
+    // res.cookie('user_id', 25);
     return res.render('home', {
         title: "firstpage"
     });
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,6 @@
+const User = require('../models/user');
+
+
 module.exports.profile = function(req,res) {
     return res.render('user_profile', {
         title: 'User Profile'
@@ -20,7 +23,27 @@ module.exports.signIn = function(req, res) {
 
 //geting the signup data
 module.exports.create = function(req, res) {
-    //TODO
+    if(req.body.password != req.body.confirm_password) {
+        return res.redirect('back');
+    }
+
+    User.findOne({email: req.body.email}, function(err, user) {
+        if(err) {
+            console.log('error in finding user');
+            return;
+        }
+        if(!user) {
+            User.create(req.body, function(err, user) {
+                if(err) {
+                    console.log('error in signing up the user');
+                    return;
+                }
+                return res.redirect('/users/sign-in');
+            })
+        } else {
+                return res.redirect('back');
+        }
+    });
 }
 
 //creating session by signin
