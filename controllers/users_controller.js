f2a39f9f6ee4eb4098c419b074f9d183ddf5af74
ujjawal1045 <90901525+ujjawal1045@users.noreply.github.com>
2022-05-27 01:10:24 +0530

    created signup and sign in page and render it by creating routes and controllers

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,15 @@ module.exports.profile = function(req,res) {
         title: 'User Profile'
     });
 }
+
+module.exports.signUp = function(req, res) {
+    return res.render('user_sign_up', {
+        title: 'BrainBook|| sign Up'
+    });
+}
+
+module.exports.signIn = function(req, res) {
+    return res.render('user_sign_in', {
+        title: 'BrainBook|| sign In'
+    });
+}
