86265e90a67fa66a13f0542f7ef3b1a0c5600948
ujjawal1045 <90901525+ujjawal1045@users.noreply.github.com>
2022-05-27 03:18:44 +0530

    showed the detail of signed user to profile page only if user is login other wise profile page is not accessible

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,9 +2,22 @@ const User = require('../models/user');
 
 
 module.exports.profile = function(req,res) {
-    return res.render('user_profile', {
-        title: 'User Profile'
-    });
+    if(req.cookies.user_id) {
+        User.findById(req.cookies.user_id, function(err, user) {
+            if(user) {
+                return res.render('user_profile', {
+                    title: 'User Profile',
+                    user:user
+                })
+            } else {
+                return res.redirect('/users/sign-in');
+            }
+        });
+    } else {
+        return res.redirect('/users/sign-in');
+
+    }
+    
 }
 
 //rendering signUp page
@@ -71,3 +84,5 @@ module.exports.createSession = function(req, res) {
         }
     });
 }
+
+
