e0db93f952a96148477cb05f0a135178536ceb84
ujjawal1045 <90901525+ujjawal1045@users.noreply.github.com>
2022-05-27 01:17:54 +0530

    created controller for crearting sessionfor the user and after signup to get data

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -4,14 +4,26 @@ module.exports.profile = function(req,res) {
     });
 }
 
+//rendering signUp page
 module.exports.signUp = function(req, res) {
     return res.render('user_sign_up', {
         title: 'BrainBook|| sign Up'
     });
 }
 
+//rendering signIn page
 module.exports.signIn = function(req, res) {
     return res.render('user_sign_in', {
         title: 'BrainBook|| sign In'
     });
+}
+
+//geting the signup data
+module.exports.create = function(req, res) {
+    //TODO
+}
+
+//creating session by signin
+module.exports.createSession = function(req, res) {
+    //TODO
 }
