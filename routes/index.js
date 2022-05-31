a6657760c57fbf00a0efd32438d5cbd7dd2ee5f9
ujjawal1045 <90901525+ujjawal1045@users.noreply.github.com>
2022-05-31 09:17:03 +0530

    created schema router controller and view for post

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ console.log('router loaded successfully');
 //02//to access home controller action in routes
 router.get('/',homeController.home);
 router.use('/users',require('./users'));
+router.use('/posts', require('./posts'));
 
 
 module.exports = router;
