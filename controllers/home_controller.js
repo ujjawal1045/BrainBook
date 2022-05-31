75cfcbb68c68df2dd9a136476316aeca1c73b5f3
ujjawal1045 <90901525+ujjawal1045@users.noreply.github.com>
2022-05-31 09:32:45 +0530

    populate the user to show the detail of post who created in home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,24 @@
+const Post = require('../models/post');
 module.exports.home = function(req,res) {
     //return res.end('<h1>Express i s up for BrainBook!</h1>');
     // console.log(req.cookies);
     // res.cookie('user_id', 25);
-    return res.render('home', {
-        title: "firstpage"
-    });
+    // Post.find({}, function(err, posts){
+    //     return res.render('home', {
+    //         title: "firstpage",
+    //         posts: posts
+    //     });
+    // });
+
+//populate user of each post
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render('home', {
+            title: "firstpage",
+            posts: posts
+        });
+    })
+
+
+    
 }
 //module.exports.acrtionname
