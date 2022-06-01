8dc6829835e739ccca4c84de87d13bd59c536674
ujjawal1045 <90901525+ujjawal1045@users.noreply.github.com>
2022-06-02 02:45:43 +0530

    displayed comment

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,24 +1,35 @@
-const Post = require('../models/post');
-module.exports.home = function(req,res) {
-    //return res.end('<h1>Express i s up for BrainBook!</h1>');
-    // console.log(req.cookies);
-    // res.cookie('user_id', 25);
-    // Post.find({}, function(err, posts){
-    //     return res.render('home', {
-    //         title: "firstpage",
-    //         posts: posts
-    //     });
-    // });
+ const Post = require('../models/post');
+ module.exports.home = function(req,res) {
+//     //return res.end('<h1>Express i s up for BrainBook!</h1>');
+//     // console.log(req.cookies);
+//     // res.cookie('user_id', 25);
+//     // Post.find({}, function(err, posts){
+//     //     return res.render('home', {
+//     //         title: "firstpage",
+//     //         posts: posts
+//     //     });
+//     // });
 
-//populate user of each post
-Post.find({}).populate('user').exec(function(err, posts){
+// populate the user of each post
+Post.find({})
+.populate('user')
+.populate({
+    path: 'comments',
+    populate: {
+        path: 'user'
+    }
+})
+.exec(function(err, posts){
     return res.render('home', {
-        title: "firstpage",
-        posts: posts
+        title: "Codeial | Home",
+        posts:  posts
     });
 })
 
-
-    
 }
 //module.exports.acrtionname
+
+
+    
+
+    
