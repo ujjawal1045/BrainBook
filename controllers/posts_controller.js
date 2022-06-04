8e497c521efa52b79ab9be724d8c68c96733de72
ujjawal1045 <90901525+ujjawal1045@users.noreply.github.com>
2022-06-05 01:07:18 +0530

    make controller acction and view for deleting comment and post from database and view

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.create = function(req, res) {
     Post.create({
@@ -12,3 +13,20 @@ module.exports.create = function(req, res) {
         return res.redirect('back');
     });
 } 
+
+module.exports.destroy = function(req, res) {
+    Post.findById(req.params.id, function(err, post) {
+        
+        if(post.user == req.user.id){
+            post.remove();
+            //return res.redirect('back');
+            Comment.deleteMany({post: req.params.id}, function(err) {
+                return res.redirect('back');
+            });
+        
+            
+        } else {
+            return res.redirect('back');
+        }
+    });
+}
