bf72424045b7c4a697a7cfa9a4c9443d5299f200
ujjawal1045 <90901525+ujjawal1045@users.noreply.github.com>
2022-06-07 01:05:33 +0530

    converted all controllers into async await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,36 +1,32 @@
  const Post = require('../models/post');
 const User = require('../models/user');
  const user = require('../models/user');
- module.exports.home = function(req,res) {
-//     //return res.end('<h1>Express i s up for BrainBook!</h1>');
-//     // console.log(req.cookies);
-//     // res.cookie('user_id', 25);
-//     // Post.find({}, function(err, posts){
-//     //     return res.render('home', {
-//     //         title: "firstpage",
-//     //         posts: posts
-//     //     });
-//     // });
+ module.exports.home = async function(req,res) {
+     try{
 
-// populate the user of each post
-Post.find({})
-.populate('user')
-.populate({
-    path: 'comments',
-    populate: {
-        path: 'user'
-    }
-})
-.exec(function(err, posts){
-    User.find({}, function(err, users){
-        return res.render('home', {
-            title: "BrainBook | Home",
-            posts:  posts,
-            all_users: users
+        // populate the user of each post
+        let posts = await Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
         });
-    });
-    
-})
+        
+            let users = await User.find({});
+                return res.render('home', {
+                    title: "BrainBook | Home",
+                    posts:  posts,
+                    all_users: users
+                });
+
+
+     } catch(err){
+         console.log('error',err);
+         return;
+     }
+
 
 }
 //module.exports.acrtionname
